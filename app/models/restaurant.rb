92bdfd8a02ea42a59017b484b762570624657058
Donald Chow <donald7290@gmail.com>
2023-05-10 11:36:04 +0900

    Completed models creation

---
+++ app/models/restaurant.rb
@@ -0,0 +1,8 @@
+class Restaurant < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true
+  CATEGORY = ["chinese", "italian", "japanese", "french", "belgian"]
+  validates :category, inclusion: { in: CATEGORY }
+
+  has_many :reviews, dependent: :destroy
+end
